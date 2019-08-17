ab7e20f24bb2eef11e622bc12a399fb1fa1e65c0
Kunal Sharda <kunalsharda@live.com>
2019-08-17 01:14:45 -0400

    Initial commit - README updated

---
+++ src/tab/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: "Richsync"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    p {
+        font-size: 20px;
+    }
+</style>
